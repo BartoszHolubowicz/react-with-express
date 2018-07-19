d64ee962c0c6c56945f2418d5aa5a05f8e20faec
gradepuxi22 <bartosz.holubowicz@gmail.com>
2018-07-19 03:41:39 +0200

    Blog's first steps

--- server.js
+++ server.js
@@ -4,32 +4,27 @@ const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const port = process.env.port || 5000;
-const dbURL = 'mongodb://admin:myd1ckisbig@ds018848.mlab.com:18848/mongo_learn';
+const dbURL = 'mongodb://gradepuxi22:!lizd412@ds243041.mlab.com:43041/bartoblog';
 
 MongoClient.connect(dbURL, { useNewUrlParser: true }, (err, database) => {
   if (err)
     return console.dir(err);
-  const db = database.db('mongo_learn');
+  const db = database.db('bartoblog');
 
-  const bookCollection = db.collection('books');
-  const testCollection = db.collection('test');
+  const postCollection = db.collection('posts');
 
-  bookCollection.find({}).toArray((err, result) => {
+  postCollection.find({}).sort({createdAt: -1, title: 1}).toArray((err, result) => {
     if (err)
       return console.dir(err);
-    app.get('/api/books', (req, res) => {
-      res.send({ books: result });
-    });
-  });
-
-  testCollection.find({}).toArray((err, result) => {
-    if (err)
-      return console.dir(err);
-    app.get('/api/test', (req, res) => {
-      res.send({ test: result });
+    app.get('/api/posts', (req, res) => {
+      res.send({ posts: result });
     });
   });
 });
+
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
